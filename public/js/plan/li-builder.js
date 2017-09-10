d52773bf985f4659bbbac8f70d3c817f8adc440c
Sharizan Mohd Redzuan <ijanzz@Sharizans-MacBook-Pro.local>
2017-09-10 20:00:14 +0800

    changes

--- public/js/plan/li-builder.js
+++ public/js/plan/li-builder.js
@@ -16,11 +16,19 @@ function liPlanList(planGuid, username, gender, problemStatement, createdDate, c
     return `<li>
         <div class="liHeader">
             <div style="float:left;position:relative;">
-                <div class="liHeader-icon" style="display:inline-block">
-                    <img src="` + imagePath + `" style="width:48px; padding:5px;" />
-                </div>
-                <div class="liHeader-creator" style="display:inline-block">` + username + `</div>
-                <!--<div>` + liIsNew + `</div>-->
+                <ul style="list-style:none;">
+                    <li style="display:inline-block;">
+                        <div class="liHeader-icon" style="display:inline-block">
+                            <img src="` + imagePath + `" style="width:48px; padding:5px;" />
+                        </div>
+                    </li>
+                    <li style="display:inline-block;">
+                        <div class="liHeader-creator" style="display:inline-block">` + username + `</div>
+                    </li>
+                    <li style="display:inline-block;">
+                        <div>` + liIsNew + `</div>
+                    </li>
+                </ul>
             </div>
             <div style="float:right">
                 <div class="dropdown">
@@ -55,6 +63,68 @@ function liPlanList(planGuid, username, gender, problemStatement, createdDate, c
 
 }; 
 
-function liPlanRootCauseList(){
-    return ``;
+function liPlanRootCauseList(rootcauseGuid, username, gender, rootcauseTitle, createdDate, currentStatus, whyCount){
+
+    var displayDate = new Date(createdDate);
+    
+    var imagePath = "";
+    if (gender === "F")
+        imagePath = "/img/user-icon-lady-64.png";
+    else 
+        imagePath = "/img/user-icon-man-64.png";
+
+    var liIsNew = "";
+    if (currentStatus === "NEW")
+        liIsNew = "<img src=\"/img/new-icon.png\" style=\"width:60px; height:30px;\">";
+
+    return `<li class="li-rootcause">
+        <div class="liContent" style="width:380px;">
+
+            <div class="liHeader">
+                <div style="float:left;position:relative;">
+                    <ul style="list-style:none;">
+                        <li style="display:inline-block;">
+                            <div class="liHeader-icon" style="display:inline-block">
+                                <img src="` + imagePath + `" style="width:48px; padding:5px;" />
+                            </div>
+                        </li>
+                        <li style="display:inline-block;">
+                            <div class="liHeader-creator" style="display:inline-block">` + username + `</div>
+                        </li>
+                        <li style="display:inline-block;">
+                            <div>` + liIsNew + `</div>
+                        </li>
+                    </ul>
+                </div>
+                <div style="float:right">
+                    <div class="dropdown">
+                        <button id="btnLiMenu" class="dropdown-toggle" type="button" data-toggle="dropdown">
+                                            <span class="caret"></span>
+                                            </button>
+                        <ul class="dropdown-menu">
+                            <li><a tabindex="-1" href="#">Edit</a></li>
+                            <li><a tabindex="-1" href="#">Remove</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="liContent" style="clear:both;">
+                <span class="liContent-title">` + rootcauseTitle + `</span>
+                <br/><br/>
+                <span class="liContent-date">2017-09-06 14:20</span>
+            </div>
+            <div class="liFooter">
+                <ul class="footer-item-left">
+                    <li>Why: <span>` + whyCount + `</span></li>
+                </ul>
+
+                <ul class="footer-item-right">
+                    <li><span class="fa fa-info-circle fa-2x" title="view root cause details"></span></li>
+                    <li><span class="fa fa-question-circle fa-2x" title="view why list" onclick="viewWhyList(this, '` + rootcauseGuid + `');"></span></li>
+                    <li><span class="fa fa-flag fa-2x" title="mark as actual root cause"></span></li>
+                </ul>
+            </div>
+
+        </div>
+    </li>`;
 };
